c3fbf062b7493dc924f1ab5acdfb846e5d469ef7
myystigri <dorian.walck@gmail.com>
2019-11-30 21:31:07 +0100

    Updates

---
+++ precache-manifest.a5dde305df90fe286ad38c83a33fc5a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2600d6ff1e97f72e27ee8c7e6e93c65",
+    "url": "/digits-to-text/index.html"
+  },
+  {
+    "revision": "32aa6651e2a44dab48b5",
+    "url": "/digits-to-text/static/css/main.b7882603.chunk.css"
+  },
+  {
+    "revision": "33e5a2ed615437efdb07",
+    "url": "/digits-to-text/static/js/2.f0a1f745.chunk.js"
+  },
+  {
+    "revision": "32aa6651e2a44dab48b5",
+    "url": "/digits-to-text/static/js/main.9faf7aa8.chunk.js"
+  },
+  {
+    "revision": "1faf2f14051699057fee",
+    "url": "/digits-to-text/static/js/runtime-main.8c1f7f68.js"
+  },
+  {
+    "revision": "2eff2ebc562365543cfa594b5c819672",
+    "url": "/digits-to-text/static/media/logo.2eff2ebc.svg"
+  }
+]);
